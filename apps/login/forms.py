cbedd62fa3154dd3ae77e0bfd1cbe80b97d19885
Jorge Espinoza <jorgeespinozaortega@gmail.com>
2021-03-31 01:28:26 -0300

    Add style

--- apps/login/forms.py
+++ apps/login/forms.py
@@ -1,8 +1,7 @@
 from django import forms
 from .models import User
-import re
 import bcrypt
-from datetime import date
+from .utils import contains_digit, contains_uppercase, is_valid_email, get_age, is_past
 
 class RegisterForm(forms.ModelForm):
 
@@ -61,11 +60,23 @@ class RegisterForm(forms.ModelForm):
 
     def clean_birthday(self):
         birthday = self.cleaned_data.get("birthday")
-        if birthday > date.today():
+        if not is_past(birthday):
             raise forms.ValidationError("Date must be past")
         if get_age(birthday) < 13:
             raise forms.ValidationError("You must be over 13 years old")
         return birthday
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get("first_name")
+        if len(first_name) < 2:
+            raise forms.ValidationError("First name needs 2 characters or more")
+        return first_name
+
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get("last_name")
+        if len(last_name) < 2:
+            raise forms.ValidationError("Last name needs 2 characters or more")
+        return last_name
 
 
 class LoginForm(forms.ModelForm):
@@ -87,7 +98,6 @@ class LoginForm(forms.ModelForm):
         cleaned_data = super(LoginForm, self).clean()
         password = cleaned_data.get("password")
         email = cleaned_data.get("email")
-        print(email, password)
 
         if not is_valid_email(email):
             raise forms.ValidationError("Not valid email")
@@ -101,29 +111,6 @@ class LoginForm(forms.ModelForm):
             raise forms.ValidationError("Wrong password")
 
         return cleaned_data
-
-
-def get_age(birthDate):
-    today = date.today()
-    date_is_before_birthday = (today.month, today.day) \
-                               < (birthDate.month, birthDate.day)
-    age = today.year - birthDate.year - date_is_before_birthday
-    return age
-
-
-def contains_digit(string):
-    RE_digit = re.compile('\d')
-    return RE_digit.search(string)
-
-
-def contains_uppercase(string):
-    RE_upper = re.compile('[A-Z]')
-    return RE_upper.search(string)
-
-
-def is_valid_email(email):
-    RE_EMAIL = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-    return RE_EMAIL.match(email)
 
 
 def password_errors(password):
---
+++ apps/login/utils.py
@@ -0,0 +1,29 @@
+import re
+from datetime import date
+
+def contains_digit(string):
+    RE_digit = re.compile('\d')
+    return RE_digit.search(string)
+
+
+def contains_uppercase(string):
+    RE_upper = re.compile('[A-Z]')
+    return RE_upper.search(string)
+
+
+def is_valid_email(email):
+    RE_EMAIL = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+    return RE_EMAIL.match(email)
+
+
+def get_age(birthDate):
+    today = date.today()
+    date_is_before_birthday = (today.month, today.day) \
+                               < (birthDate.month, birthDate.day)
+    age = today.year - birthDate.year - date_is_before_birthday
+    return age
+
+
+def is_past(a_date):
+    return a_date < date.today() 
+
