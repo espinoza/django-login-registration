bf1f1582ba857aeed346257db0d74900baf06e77
Jorge Espinoza <jorgeespinozaortega@gmail.com>
2021-04-01 03:21:56 -0300

    Fix margins

--- apps/login/static/js/script.js
+++ apps/login/static/js/script.js
@@ -1,45 +1,45 @@
 $(document).ready(function() {
 
-	$("#id_email_registration").focusout("input", validateEmail);
-	$("#id_email_registration").after("<div id='email-errors' class='text-danger'></div>");
+  $("#id_email_registration").focusout("input", validateEmail);
+  $("#id_email_registration").after("<div id='email-errors' class='text-danger'></div>");
 
 })
 
 function validateEmail() {
 
-	let formData = $("#register-form").serialize();
+  let formData = $("#register-form").serialize();
 
-	$.ajax({
+  $.ajax({
 
-		type: "POST",
-		url: "/validate_email",
-		data: formData,
-		dataType: 'JSON',
+    type: "POST",
+    url: "/validate_email",
+    data: formData,
+    dataType: 'JSON',
 
-		success: function(response) {
+    success: function(response) {
 
-			$("#email-errors").text("");
+      $("#email-errors").text("");
 
-			if (response.errors) {
-				errorsAsP = "";
-				for (let i = 0; i < response.errors.length; i++) {
-					error = response.errors[i];
-					console.log(error);
-					errorsAsP += "<p><strong>" + error + "</strong></p>";
-				}
-				$("#email-errors").html(errorsAsP);
-			}
+      if (response.errors) {
+        errorsAsP = "";
+        for (let i = 0; i < response.errors.length; i++) {
+          error = response.errors[i];
+          console.log(error);
+          errorsAsP += "<p><strong>" + error + "</strong></p>";
+        }
+        $("#email-errors").html(errorsAsP);
+      }
 
-		},
+    },
 
-		error: function (response) {
+    error: function (response) {
 
-			let message = "There is no response from server";
-			$("#email-errors").text(message);
-			console.log(response)
+      let message = "There is no response from server";
+      $("#email-errors").text(message);
+      console.log(response)
 
-		}
+    }
 
-	});
+  });
 
 }
